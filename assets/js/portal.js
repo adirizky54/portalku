06a5ab698ee9d4ba8aba42d36a58ee14f8b09f11
Adi Rizky <adirizky54@gmail.com>
2019-01-04 16:34:29 +0700

    Ganti Header

--- assets/js/portal.js
+++ assets/js/portal.js
@@ -1,87 +1,30 @@
 window.onresize = displayWindowSize;
 window.onload = displayWindowSize;
 
-function displayWindowSize() {
-  myWidth = window.innerWidth;
-  if(myWidth >= 768){
-    var subnavShowTreshold = 10; // In pixels
-    var $subnavWrapper = $('#portal-subnav-wrapper');
-    var $subnav = $('#portal-subnav');
-
-    var curSubnavHeight;
-
-    function updateDimensions() {
-      var height = $subnav.outerHeight();
-      if (height !== curSubnavHeight) {
-        curSubnavHeight = height;
-        $subnavWrapper.height(height);
-        $subnav.css('top', (-1 * height) - 14);
-      }
-    }
-
-    updateDimensions();
-    $(window).on('resize', updateDimensions);
-
-    // Subnav behaviour
-
-    var lastScrollPos = 0;
-    var scrollUpDelta = 0;
-
-    function updateSubnavPosition() {
-      var fixTopPoint = $subnavWrapper.offset().top;
-      var fixBottomPoint = fixTopPoint + curSubnavHeight;
-      var scrollTop = $(document).scrollTop();
-
-      if (!$subnav.hasClass('subnav-fixed')) {
-        if (scrollTop > fixBottomPoint) {
-          $subnav.addClass('subnav-fixed');
-          scrollUpDelta = 0;
-        }
-
-      } else {
-        if (scrollTop < fixTopPoint) {
-          $subnav.removeClass('subnav-fixed subnav-shown');
-
-        } else if (scrollTop < fixBottomPoint) {
-          $subnav.addClass('subnav-shown');
-          $("#search-box").removeClass('show');
-          $("#logo-image").removeClass('d-block');
-        } else if (scrollTop < lastScrollPos) {
-          scrollUpDelta += lastScrollPos - scrollTop;
-
-          if (scrollUpDelta > subnavShowTreshold) {
-            $subnav.addClass('subnav-shown');
-            $("#logo-image").addClass('d-block');
-          }
-
-        } else {
-          $subnav.removeClass('subnav-shown');
-          $("#logo-image").removeClass('d-block');
-          scrollUpDelta = 0;
-        }
-      }
-
-      lastScrollPos = scrollTop;      
-    }
-    
-    updateSubnavPosition();
-    $(document).on('scroll', updateSubnavPosition);
-    $(window).on('resize', updateSubnavPosition);
-  } else {
-    $subnavWrapper.removeAttr("style");
-    $subnav.removeAttr("style");
-    $subnav.removeClass('subnav-fixed subnav-shown');
-    $("#logo-image").removeClass('d-block');
-  }  
-}
-
-$('.navbar-search-input > input').on('focus', function() {
-  $('.navbar-search-box').addClass('active');
+$('#portal-nav').scrollToFixed({
+  maxWidth: 767
 });
 
-$('.navbar-search-cancel').on('click', function(e) {
-  e.preventDefault();
-  $('.navbar-search-box').removeClass('active');
+function displayWindowSize() {
+  myWidth = window.innerWidth;
+  if(myWidth <= 767) {
+    $(window).scroll(function() {
+      if ($(this).scrollTop()>0) {
+        $('#top-banner').removeClass('d-block').addClass('d-none');
+      }
+      else {
+        $('#top-banner').removeClass('d-none').addClass('d-block');
+      }
+    });
+  } else {}
+}
+
+$('#portal-subnav-wrapper').scrollToFixed({
+  minWidth: 768,
+  zIndex: 1092,
+  fixed: function() { $(this).find('#logo-image').removeClass('d-none').addClass('d-block'); },
+  postFixed: function() { $(this).find('#logo-image').removeClass('d-block').addClass('d-none'); },
+  unfixed: function() { $(this).find('#logo-image').removeClass('d-block').addClass('d-none'); },
 });
 
 $('.trending-post').easyTicker({
